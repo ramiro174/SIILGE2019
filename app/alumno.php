0eaf4b7464d88884f4bd1b6682a5d78f4bc59956
Ramiro Esquivel Duran <ramiro174@gmail.com>
2019-03-05 07:43:26 -0600

    agregue un modelo

---
+++ app/alumno.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class alumno extends Model
+{
+    protected  $primaryKey="id";
+    protected  $table="alumnos";
+    public $timestamps=false;
+    public $fillable=["nombre"];
+    
+}
