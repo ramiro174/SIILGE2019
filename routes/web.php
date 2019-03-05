a6544935bbc80d25b3590dad73856820e1061600
Ramiro Esquivel Duran <ramiro174@gmail.com>
2019-03-04 22:26:34 -0600

    ad router

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,40 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+    
+    
+    Route::get("/lista",function(){
+        $nombre=["Juan","Pedro","Alex"];
+        $apellido=["Esquivel","Solis","Romero"];
+        $telefono=["871-176-60-65","871-130-30-12","871-234-33-22"];
+        $lista=array();
+        for($x=0;$x<=20;$x++){
+            $lista[]=array("id"=>$x+1,"Nombre"=>$nombre[rand (0,2)],"Apellido"=> $apellido[rand (0,2)],"Edad"=>rand (1,50), "Telefono"=> $telefono[rand(0,2)]   );
+        }
+        return response($lista, 200)
+            ->header('Content-Type', 'application/json')
+            ->header('expires','Fri, 15 Feb 2019 19:52:41 GMT');
+    });
+    
+    
+    
+    Route::post("/nombre",function(Request $request){
+        
+        
+        
+        
+        if($request->exists('Nombre')){
+            $nombre= $request->input('Nombre');
+        }else{
+            $nombre="Sin Nombre";
+        }
+        
+        
+        
+        $m=["Mensaje"=>"Hola :)".$nombre];
+        
+        return response($m, 200)
+            ->header('Content-Type', 'application/json')
+            ->header('expires','Fri, 15 Feb 2019 19:52:41 GMT');
+        
+    });
